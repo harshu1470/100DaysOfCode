90e3cd1f4ca97a466df9bd77a1c551061b4719cc
harshu1470 <harshit1470@gmail.com>
2020-05-03 03:44:54 +0530

    1st day of 100DaysOfCode

---
+++ 100DaysOfCode/Day1/menu_item.py
@@ -0,0 +1,16 @@
+class MenuItem:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price 
+    def info(self):
+        return self.name + ' :  $' + str(self.price) 
+    def get_total_price(self,count):
+        total_price = self.price*count
+        if count >=3:
+            discount=0.9
+            total_price*=discount
+        return round(total_price)
+
+
+
+
