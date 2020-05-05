f2c59ba79c7dd946efbab2e9eb870751c2cfd4ae
harshu1470 <harshit1470@gmail.com>
2020-05-05 20:03:34 +0530

    Day3 of 100DaysOfCode

---
+++ 100DaysOfCode/Day3/array_operation.py
@@ -0,0 +1,15 @@
+#array opration's
+from array import *
+array1 = [1,2,4,5,6,7]
+a1= array('i',array1)
+print(a1[3])
+a1.insert(1,20)
+print(a1.index(20))
+a2 = array('i',[2,4,22,1,2,3])
+a1[4]= 80
+for c in a1:
+    print(c,'\n')
+a3=a1 + a2
+for i in a3:
+    print(i)
+
---
+++ 100DaysOfCode/Day3/dictionary.py
@@ -0,0 +1,6 @@
+#Dictionary operation's
+
+dict ={"Name": "harshit" ,"age":20 , "college" : "SKIT"}
+print(dict.get)
+print(dict.values())
+
